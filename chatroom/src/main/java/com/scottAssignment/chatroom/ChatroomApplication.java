986d0fde5ee988cd5f38af7e6ca471314997c35c
ScottyP666 <Scott_M_G@live.com>
2022-05-23 08:23:21 -0700

    init stuff

---
+++ chatroom/src/main/java/com/scottAssignment/chatroom/ChatroomApplication.java
@@ -0,0 +1,13 @@
+package com.scottAssignment.chatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatroomApplication.class, args);
+	}
+
+}
